9f46388d3d417042e9c9cc404eb0a763afd8ef60
Richmond <ryeboah@laisar.com>
2022-11-19 19:41:51 -0500

    Create python_101.py

---
+++ python_101.py
@@ -0,0 +1,11 @@
+# Intro to Git
+# Richmond Yeboah
+
+students = ["Bryan", "Richmond", "Hannah ", "Joseph", "Sierra"]
+
+def count():
+    for student in students:
+        print(student)
+    else: 
+        print("Today's a holiday, the students are on break!")
+count()
